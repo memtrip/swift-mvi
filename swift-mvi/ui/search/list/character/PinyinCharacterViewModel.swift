8af21ad8e1ee70c10b962b9e65bc0afa1482c43f
Samuel Kirton <samkirton@me.com>
2018-07-06 22:05:28 +0100

    * updated the default search term for the character search to be 拼音

--- swift-mvi/ui/search/list/character/PinyinCharacterViewModel.swift
+++ swift-mvi/ui/search/list/character/PinyinCharacterViewModel.swift
@@ -4,6 +4,6 @@ import RxSwift
 class PinyinCharacterViewModel : PinyinListViewModel {
     
     init() {
-        super.init(search: CharacterSearch(), defaultSearchTerm: "汉语")
+        super.init(search: CharacterSearch(), defaultSearchTerm: "拼音")
     }
 }
